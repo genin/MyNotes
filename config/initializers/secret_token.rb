4b6e6a718c663f048789a297182c2fc4eddc2aad
genin <genin@dosisoft.fr>
2012-05-22 16:46:00 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyNotes::Application.config.secret_token = 'c9e148ec5e06a5543f0213223b95063046f4030b58e7b1f04caef227af9684081ce4a0f833c15340d10c2549e67e2032d7050800136976f00e3503718fb48c2c'
